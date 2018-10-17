710fc0c6dd957f8553e8e339e886775ed0216b41
Ramon Daniel Emiliani Mejia <ramon@ramons-mbp.sf.umontreal.ca>
2018-10-17 16:11:34 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project 1 for IFT6758 (Data Science).',
+    author='Emilio Botero, Ramon Emiliani, Alejandro Posada, Joseph Viviano',
+    license='MIT',
+)
